0fbec91cd283e90695e75934329cbdeb48a94de3
Luc Dang <quanglucdang2804@gmail.com>
2019-12-21 21:04:45 +0700

    create spec expect create 2 hotels with 2 object from response

---
+++ app/services/sanitizing_hotel/supplier1.rb
@@ -0,0 +1,45 @@
+module SanitizingHotel
+  class Supplier1
+
+    CONCATENATE_WORDS = ['wifi']
+
+    def execute
+      hotels = fetch_request
+      hotels.map do |hotel_params|
+        find_or_create_hotel!(hotel_params)
+      end
+    end
+
+    private
+
+    def find_or_create_hotel!(hotel_params)
+      hotel = Hotel.find_or_initialize_by(hotel_id: hotel_params['Id'])
+      params = {
+        hotel_id: hotel_params['Id'],
+        name: hotel_params['Name'].strip,
+        description: hotel_params['Description'].strip,
+        address: hotel_params['Address'].strip,
+        longitude: hotel_params['Longitude'],
+        latitude: hotel_params['Latitude'],
+        postal_code: hotel_params['PostalCode'].strip,
+        destination_id: hotel_params['DestinationId'],
+        city: hotel_params['City'].strip,
+        country: hotel_params['Country'].strip,
+      }
+      hotel.update_attributes!(params)
+      hotel.reload
+    end
+
+    def url
+      'https://api.myjson.com/bins/gdmqa'
+    end
+
+    def fetch_request
+      response = ::RestClient::Request.execute(method: :get, url: url)
+      JSON.parse(response.body)
+    rescue StandardError => error
+      message = 'Can not fetch data from Supplier1'
+      Rails.logger.error "#{message}  #{error}"
+    end
+  end
+end
