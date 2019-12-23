71138ea4f668657553ddb2bcc222fe98f98628ed
Luc Dang <quanglucdang2804@gmail.com>
2019-12-23 07:34:13 +0700

    update travis ci for auto test and deploy

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'hotels/sync_data' => 'hotels#sync_data'
+  get '/' => 'hotels#index'
   resources :hotels
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
